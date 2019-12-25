6590ed87b434e20a91d06775a0ef91c59f3935bb
Babbaj <babbaj45@gmail.com>
2019-12-25 12:24:06 -0500

    ok this is epic

--- src/main/java/spawn/mason/Main.java
+++ src/main/java/spawn/mason/Main.java
@@ -11,9 +11,10 @@ import java.io.File;
 import java.io.IOException;
 import java.util.*;
 import java.util.List;
-import java.util.function.Consumer;
+import java.util.concurrent.TimeUnit;
 import java.util.function.ToIntFunction;
 import java.util.stream.Collectors;
+import java.util.stream.LongStream;
 import java.util.stream.Stream;
 import java.util.stream.StreamSupport;
 
@@ -22,70 +23,137 @@ public class Main {
     private static final int SZ = 144;
 
     public static void main(String[] args) throws IOException {
+        final long now = System.currentTimeMillis();
+
         MongoClient mongoClient = MongoClients.create("mongodb://babbaj:babbaj1@ds023448.mlab.com:23448/heroku_7scmkkrf");
         //MongoDatabase database = mongoClient.getDatabase("masonweb");
         MongoDatabase database = mongoClient.getDatabase("heroku_7scmkkrf");
         MongoCollection<Document> collection = database.getCollection("regions");
-        FindIterable<Document> documents = collection.find();
+        FindIterable<Document> iterable = collection.find();
 
-        final BufferedImage image = renderImage(documents);
-        ImageIO.write(image, "png", new File("output.png"));
+        final List<Section> sections =  StreamSupport.stream(iterable.spliterator(), false)
+            .map(Section::fromDocument)
+            .collect(Collectors.toList());
+
+        final long start = startTime(sections);
+        final long end = endTime(sections);
+        int i = 0;
+        final File outDir = new File("output");
+        outDir.mkdir();
+        for (File f : outDir.listFiles()) { f.delete(); }
+
+        for (long t = start; ; t += TimeUnit.HOURS.toMillis(3)) {
+            final BufferedImage image = renderImage(sections, t);
+            final String name = String.format("%03d", i);
+            final File f = new File("output/" + name + ".png");
+            System.out.println(i);
+            ImageIO.write(image, "png", f);
+            i++;
+            if (t > end) break;
+        }
+
+        System.out.println((System.currentTimeMillis() - now) / 1000 + "s");
     }
 
     static class IntPair {
         final int x;
-        final int y;
-        IntPair(int x, int y) {
+        final int z;
+        IntPair(int x, int z) {
             this.x = x;
-            this.y = y;
+            this.z = z;
         }
     }
 
-    private static BufferedImage renderImage(FindIterable<Document> iterable) {
-        List<Document> documents =  StreamSupport.stream(iterable.spliterator(), false)
-            .collect(Collectors.toList());
-        final int maxX = maxDoc(documents.stream(), true, false).get();
-        final int minX = maxDoc(documents.stream(), true, true).get();
-        final int maxY = maxDoc(documents.stream(), false, false).get();
-        final int minY = maxDoc(documents.stream(), false, true).get();
+    public static class Section extends IntPair {
+        final long startTime;
+        final long endTime;
+        final boolean[][] obsidian;
+
+        public Section(int x, int z, long startTime, long endTime, boolean[][] obsidian) {
+            super(x, z);
+            this.startTime = startTime;
+            this.endTime = endTime;
+            this.obsidian = obsidian;
+        }
+
+        public static Section fromDocument(Document doc) {
+            final boolean[][] obsidian = parseObsidian(doc.getString("regionData"));
+            final int x = doc.getInteger("X");
+            final int z = doc.getInteger("Z");
+            final List<Document> history = doc.getList("history", Document.class);
+            // TODO: figure out fastest possible time
+            final long start = getTimestamps(history).min().getAsLong();
+            final long end = getTimestamps(history).max().getAsLong();
+
+            return new Section(x, z, start, end, obsidian);
+        }
+
+        private static LongStream getTimestamps(List<Document> history) {
+            return history.stream()
+                .filter(d -> d.getString("status").equals("PLACING") || d.getString("status").equals("DONE"))
+                .mapToLong(d -> d.get("timestamp", Date.class).getTime());
+        }
+    }
+
+
+    private static BufferedImage renderImage(List<Section> sections, long time) {
+        final int maxX = maxInt(sections.stream(), true, false).get();
+        final int minX = maxInt(sections.stream(), true, true).get();
+        final int maxZ = maxInt(sections.stream(), false, false).get();
+        final int minZ = maxInt(sections.stream(), false, true).get();
         final int width = maxX - minX;
-        final int height = maxY - minY;
+        final int height = maxZ - minZ;
 
         // approximately 11k x 11k
         final BufferedImage image = new BufferedImage((width + 1) * SZ, (height + 1) * SZ, BufferedImage.TYPE_BYTE_BINARY);
         fillWhite(image);
 
-        documents.forEach((Consumer<Document>)doc -> {
-            final boolean[][] obsidian = parseObsidian(doc.getString("regionData"));
-            final IntPair pos = parseId(doc.getString("sectionId"));
-            final int baseX = (pos.x + Math.abs(minX)) * SZ;
-            final int baseY = (pos.y + Math.abs(minY)) * SZ;
+        sections.forEach(sec -> {
+            final int baseX = (sec.x + Math.abs(minX)) * SZ;
+            final int baseZ = (sec.z + Math.abs(minZ)) * SZ;
 
-
-            for (int i = 0; i < SZ; i++)
-            for (int j = 0; j < SZ; j++) {
-                final boolean b = obsidian[i][j];
-                image.setRGB(baseX + i, baseY + j, b ? 0 : 0xFF_FF_FF_FF);
-             }
+            renderSection(image, sec, baseX, baseZ, time);
         });
 
         return image;
     }
 
+    private static void renderSection(BufferedImage image, Section sec, int x, int z, long time) {
+        if (time < sec.startTime) return;
+
+        final boolean[][] obsidian = sec.obsidian;
+        for (int i = 0; i < SZ; i++)
+            for (int j = 0; j < SZ; j++) {
+                final boolean b = obsidian[i][j];
+                image.setRGB(x + i, z + j, b ? 0 : 0xFF_FF_FF_FF);
+            }
+    }
+
     private static void fillWhite(BufferedImage image) {
         Graphics2D graphics = image.createGraphics();
 
-        graphics.setPaint(new Color ( 255, 255, 255 ));
+        graphics.setPaint(new Color (255, 255, 255));
         graphics.fillRect(0, 0, image.getWidth(), image.getHeight());
     }
 
-    private static Optional<Integer> maxDoc(Stream<Document> documents, boolean x, boolean reversed) {
-        return maxInt(documents.map(doc -> parseId(doc.getString("sectionId"))), x, reversed);
+    private static long startTime(List<Section> sections) {
+        return sections.stream()
+            .mapToLong(s -> s.startTime)
+            .min()
+            .getAsLong();
     }
 
+    private static long endTime(List<Section> sections) {
+        return sections.stream()
+            .mapToLong(s -> s.endTime)
+            .max()
+            .getAsLong();
+    }
+
+
     // pretty dumb api lmao
-    private static Optional<Integer> maxInt(Stream<IntPair> stream, boolean x, boolean reversed) {
-        final ToIntFunction<IntPair> getter = x ? p -> p.x : p -> p.y;
+    private static Optional<Integer> maxInt(Stream<? extends IntPair> stream, boolean x, boolean reversed) {
+        final ToIntFunction<IntPair> getter = x ? p -> p.x : p -> p.z;
         Comparator<IntPair> comp = Comparator.comparingInt(getter);
         if (reversed) comp = comp.reversed();
         return stream.max(comp).map(getter::applyAsInt);
